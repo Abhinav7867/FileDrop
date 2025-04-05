998f9fc0c86801c5a4ac18b073361dc25bea1705
Abhinav Jha <itisbpjha@hotmail.com>
2025-04-06 03:10:16 +0530

    polishing

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -6,18 +6,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        
         let mainViewController = MainViewController()
         let navigationController = UINavigationController(rootViewController: mainViewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        
         window?.rootViewController = navigationController
+        window?.backgroundColor = .systemBackground
+        window?.tintColor = .systemBlue
         window?.makeKeyAndVisible()
+        
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle (disabled)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 } 
